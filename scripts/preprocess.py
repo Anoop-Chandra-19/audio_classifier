da0fc2f4677afdfc074076c1cfde1395d2e5f9f7
Anoop <anoopchandraparampalli@gmail.com>
2025-04-20 20:55:26 -0400

    preprocess script

---
+++ scripts/preprocess.py
@@ -0,0 +1,45 @@
+# This script processes audio files in the specified directory, 
+# computes their log-mel spectrograms, and saves them as PyTorch tensors.
+
+import os
+import sys
+import torch
+from tqdm import tqdm
+from src.utils import load_audio, compute_log_mel_spectrogram
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+
+RAW_AUDIO_DIR = "data/raw/fma_small"
+OUT_DIR = "data/processed"
+audio_paths = []
+os.makedirs(OUT_DIR, exist_ok=True)
+
+# Collect all audio file paths
+# in the specified directory and its subdirectories
+# and store them in a list
+# The audio files should be in .wav or .mp3 format
+
+for root, _, files in os.walk(RAW_AUDIO_DIR):
+    for fname in files:
+        if fname.lower().endswith((".wav", ".mp3")):
+            audio_paths.append(os.path.join(root, fname))
+
+# Process each audio file
+# Compute the log-mel spectrogram
+# and save it as a PyTorch tensor
+# The output file name should be the same as the input file name
+# but with a .pt extension
+
+for in_path in tqdm(audio_paths, desc="Processing audio files"):
+    track_id = os.path.splitext(os.path.basename(in_path))[0]  # eg. "000002"
+    try:
+        waveform = load_audio(in_path)
+        spec = compute_log_mel_spectrogram(waveform)
+        out_path = os.path.join(OUT_DIR, f"{track_id}.pt")
+        torch.save(spec, out_path)
+    except Exception as e:
+        print(f"Failed to process {track_id}: {e}")            
+
+
+print("All files processed.")
+
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,16 @@ import torch
 import librosa
 
 def load_audio(path: str, sample_rate: int = 16000) -> np.ndarray:
+    '''
+    Loads an audio file and returns the waveform as a numpy array.
+    
+    Args:
+        path (str): Path to the audio file.
+        sample_rate (int): Sample rate for loading the audio. Default is 16000 Hz.
+    
+    Returns:
+        np.ndarray: Waveform of the audio file.
+    '''
     y, sr = librosa.load(path, sr=sample_rate, mono=True)
     return y
 
@@ -13,6 +23,19 @@ def compute_log_mel_spectrogram(
         n_fft: int = 1024, 
         hop_length: int = 512
         ) -> torch.Tensor:
+    '''
+    Computes the log-mel spectrogram of a waveform.
+    
+    Args:
+        waveform (np.ndarray): Waveform of the audio file.
+        sample_rate (int): Sample rate for loading the audio. Default is 16000 Hz.
+        n_mels (int): Number of mel bands to generate. Default is 128.
+        n_fft (int): Length of the FFT window. Default is 1024.
+        hop_length (int): Number of samples between frames. Default is 512.
+    
+    Returns:
+        torch.Tensor: Log-mel spectrogram of the audio file.
+    '''
     # power spectrogram
     S = librosa.feature.melspectrogram(
         y=waveform, 
